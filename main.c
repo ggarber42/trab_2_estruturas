e4ddc41a7de60a042c56b1f8d4ed7e31ab4711eb
ggarber42 <ggarber@protonmail.com>
2021-08-05 22:44:35 -0300

    comentarios removidos

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Represent a contato of the doubly linked list
-
 struct contato
 {
     char nome[50];
@@ -13,59 +11,43 @@ struct contato
     struct contato *next;
 };
 
-//Represent the head and tail of the doubly linked list
 struct contato *head, *tail = NULL;
 
-//addcontato() will add a contato to the list
 void addcontato(struct contato *pessoa)
 {
-    //Create a new contato
     struct contato *newcontato = (struct contato *)malloc(sizeof(struct contato));
     strcpy(newcontato->nome, pessoa->nome);
     strcpy(newcontato->email, pessoa->email);
     strcpy(newcontato->telefone, pessoa->telefone);
-    //If list is empty
     if (head == NULL)
     {
-        //Both head and tail will point to newcontato
         head = tail = newcontato;
-        //head's previous will point to NULL
         head->previous = NULL;
-        //tail's next will point to NULL, as it is the last contato of the list
         tail->next = NULL;
     }
     else
     {
-        //newcontato will be added after tail such that tail's next will point to newcontato
         tail->next = newcontato;
-        //newcontato's previous will point to tail
         newcontato->previous = tail;
-        //newcontato will become new tail
         tail = newcontato;
-        //As it is last contato, tail's next will point to NULL
         tail->next = NULL;
     }
 }
 
-//sortList() will sort the given list in ascending order
 void sortList()
 {
     struct contato *current = NULL, *index = NULL;
     char temp[50];
-    //Check whether list is empty
     if (head == NULL)
     {
         return;
     }
     else
     {
-        //Current will point to head
         for (current = head; current->next != NULL; current = current->next)
         {
-            //Index will point to contato next to current
             for (index = current->next; index != NULL; index = index->next)
             {
-                //If current's nome is greater than index's nome, swap the nome of current and index
                 int n = 0;
                 while (n < strlen(current->nome))
                 {
@@ -86,10 +68,8 @@ void sortList()
     }
 }
 
-//display() will print out the contatos of the list
 void display()
 {
-    //contato current will point to head
     struct contato *current = head;
     if (head == NULL)
     {
@@ -98,7 +78,6 @@ void display()
     }
     while (current != NULL)
     {
-        //Prints each contato by incrementing pointer.
         printf("%s ", current->nome);
         current = current->next;
     }
@@ -108,13 +87,10 @@ void display()
 void getContato(int posicao)
 {
     struct contato *current = head;
-    /* if list in NULL or invalid position is given */
     if (posicao <= 0 || head == NULL)
     {
         return;
     }
-    /* traverse up to the node at position 'n' from
-       the beginning */
     int i;
     for (int i = 1; current != NULL && i < posicao; i++)
     {
@@ -127,47 +103,36 @@ void getContato(int posicao)
 void deleteContato(int posicao)
 {
     struct contato *current = head;
-    /* if list in NULL or invalid position is given */
     if (posicao <= 0 || head == NULL)
     {
         return;
     }
-    /* traverse up to the node at position 'n' from
-       the beginning */
     int i;
     for (int i = 1; current != NULL && i < posicao; i++)
     {
         current = current->next;
     }
-
-    /* if 'n' is greater than the number of nodes
-       in the doubly linked list */
     if (current == NULL)
     {
         return;
     }
-    /* delete the node pointed to by 'current' */
-    // deleteNode(head_ref, current);
-
-    /* base case */
     if (head == NULL || current == NULL)
+    {
         return;
+    }
 
-    /* If node to be deleted is head node */
     if (head == current)
+    {
         head = current->next;
-
-    /* Change next only if node to be deleted is NOT
-       the last node */
+    }
     if (current->next != NULL)
+    {
         current->next->previous = current->previous;
-
-    /* Change prev only if node to be deleted is NOT
-       the first node */
+    }
     if (current->previous != NULL)
+    {
         current->previous->next = current->next;
-
-    /* Finally, free the memory occupied by del*/
+    }
     free(current);
 };
 
